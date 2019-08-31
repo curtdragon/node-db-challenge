ab68348025a307c5cf59287b9f45ab91c88fb900
curtdragon <tealturboser@gmail.com>
2019-08-30 19:21:08 -0500

    All dependencies installed. Server, Index, db-config, knexfile and migration file complete. Seeds, router and model file created and work in progress

---
+++ data/migrations/20190830184807_create-tables.js
@@ -0,0 +1,57 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("projects", table => {
+        table.increments();
+        table.string("name")
+            .notNullable()
+            .unique();
+        table.string("description");
+        table.boolean("completed")
+            .notNullable()
+            .defaultTo(false);
+    })
+    .createTable("resources", table => {
+        table.increments();
+        table.string("name")
+            .unique()
+            .notNullable();
+        table.string("description");
+    })
+    .createTable("tasks", table => {
+        table.increments();
+        table.string("description")
+            .notNullable();
+        table.string("notes")
+        table.integer("project_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("projects")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+    })
+    .createTable("project_tasks", table => {
+        table.integer("project_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("projects")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+        table.integer("task_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("tasks")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists("project_tasks")
+        .dropTableIfExists("tasks")
+        .dropTableIfExists("resources")
+        .dropTableIfExists("projects")  
+};
